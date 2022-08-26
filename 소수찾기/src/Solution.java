7b2c8a7361ee9a3cfcb73f7515fd67e3c14944b4
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-08-26 11:30:47 +0900

    Refactor 소수찾기
    Refactor 소수찾기.

--- 소수찾기/src/Solution.java
+++ 소수찾기/src/Solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
+        int[] primeNumbers = new int[n];
+        int j;
 
-        for (int i = 1; i < n + 1; i += 1) {
-            int half = i / 2;
-            int j;
-
-            for (j = 2; j <= half; j += 1){
-                if (i % j == 0) {
+        for (int i = 2; i <= n; i += 1) {
+            for (j = 0; j < answer; j += 1) {
+                if (i % primeNumbers[j] == 0) {
                     break;
                 }
             }
 
-            if (j == half + 1) {
+            if (j == answer) {
+                primeNumbers[answer] = i;
                 answer += 1;
             }
         }
