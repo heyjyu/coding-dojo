cd8c62f54edf6bf6d0bf1e893ec11e91b65a548f
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-08-26 12:25:46 +0900

    Use isPrime variable
    Use isPrime variable.

--- 소수찾기/src/Solution.java
+++ 소수찾기/src/Solution.java
@@ -1,17 +1,28 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        int[] primeNumbers = new int[n];
-        int j;
+        int[] primeNumbers = new int[n - 1];
+        boolean isPrime = true;
 
         for (int i = 2; i <= n; i += 1) {
-            for (j = 0; j < answer; j += 1) {
+            for (int j = 0; j < answer; j += 1) {
+                isPrime = false;
+
                 if (i % primeNumbers[j] == 0) {
                     break;
                 }
+
+                if (primeNumbers[j] > Math.sqrt(i)) {
+                    primeNumbers[answer] = i;
+                    answer += 1;
+
+                    break;
+                }
+
+                isPrime = true;
             }
 
-            if (j == answer) {
+            if (isPrime) {
                 primeNumbers[answer] = i;
                 answer += 1;
             }
