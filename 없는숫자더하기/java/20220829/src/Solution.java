e9102742be3efd6cea9600fc7d0a273bd09fa318
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-08-29 09:02:00 +0900

    Add non-existing numbers
    Add non-existing numbers.

---
+++ 없는숫자더하기/java/20220829/src/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 9 * 10 / 2;
+
+        for (int number : numbers) {
+            answer -= number;
+        }
+
+        return answer;
+    }
+}
