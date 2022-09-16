f59a03e93f16d6e02718d4bf026de64daa8eecbd
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-09-16 10:29:28 +0900

    Add test for converting to base 10
    Add test for converting to base 10.

--- 3진법뒤집기/java/20220916/src/Solution.java
+++ 3진법뒤집기/java/20220916/src/Solution.java
@@ -3,4 +3,23 @@ class Solution {
         int answer = 0;
         return answer;
     }
+
+    public String convertTo3reversed(int number) {
+        String convertedValue = "";
+
+        int quotient = number / 3;
+        int remainder = number % 3;
+        convertedValue += String.valueOf(remainder);
+
+        while (quotient >= 3) {
+            remainder = quotient % 3;
+            quotient = quotient / 3;
+
+            convertedValue += remainder;
+        }
+
+        convertedValue += String.valueOf(quotient);
+
+        return convertedValue;
+    }
 }
--- 3진법뒤집기/java/20220916/src/SolutionTest.java
+++ 3진법뒤집기/java/20220916/src/SolutionTest.java
@@ -5,10 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest {
 
     @Test
-    void convertTo3() {
+    void convertTo3reversed() {
         Solution solution = new Solution();
 
-        assertEquals(10, solution.convertTo3(3));
-        assertEquals(1200, solution.convertTo(45));
+        assertEquals("01", solution.convertTo3reversed(3));
+        assertEquals("0021", solution.convertTo3reversed(45));
+        assertEquals("22111", solution.convertTo3reversed(125));
+    }
+
+    @Test
+    void convertTo10() {
+        Solution solution = new Solution();
+
+        assertEquals(7, solution.convertTo10("0021"));
+        assertEquals(229, solution.convertTo10("22111"));
     }
 }
