8e08bdc31660e816dcb7b069370aae30cbea3cf2
heyjyu <jiwoosmile@yonsei.ac.kr>
2022-10-21 09:37:00 +0900

    Convert to Caesar code in java
    Convert to Caesar code in java.

--- 시저암호/java/20221021/src/Solution.java
+++ 시저암호/java/20221021/src/Solution.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Solution {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Solution {
+    public String solution(String s, int n) {
+        String answer = convert(s, n);
+        return answer;
+    }
+
+    public String moveCharacter(String character, int times) {
+        char lastCharacter = character.equals(character.toLowerCase())
+                ? 'z'
+                : 'Z';
+
+        return character.charAt(0) + times > lastCharacter
+                ? String.valueOf((char) (character.charAt(0) + times - 26))
+                : String.valueOf((char) (character.charAt(0) + times));
+    }
+
+    public String convert(String string, int times) {
+        Pattern pattern = Pattern.compile("\\w");
+        Matcher matcher = pattern.matcher(string);
+
+        return matcher.replaceAll(character -> moveCharacter(String.valueOf(character.group()), times));
+    }
 }
